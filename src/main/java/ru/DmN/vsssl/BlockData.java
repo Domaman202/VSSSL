59b3988dedd3490e842d6470f2d601bf84f74d33
Domaman202 <vip.domaman@mail.ru>
2022-07-05 00:57:16 +0700

    flex

--- src/main/java/ru/DmN/vsssl/BlockData.java
+++ src/main/java/ru/DmN/vsssl/BlockData.java
@@ -1,17 +1,44 @@
 package ru.DmN.vsssl;
 
-import java.io.Serializable;
+import net.minecraft.block.BlockState;
+import net.minecraft.state.property.Property;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class BlockData implements Serializable {
     public String id;
     public int xOffset;
     public int yOffset;
     public int zOffset;
+    public Map<String, Object> properties;
 
-    public BlockData(String id, int xOffset, int yOffset, int zOffset) {
-        this.id = id;
+    public BlockData() {
+    }
+
+    public BlockData(int xOffset, int yOffset, int zOffset, BlockState state) {
+        this.id = Registry.BLOCK.getId(state.getBlock()).getPath();
         this.xOffset = xOffset;
         this.yOffset = yOffset;
         this.zOffset = zOffset;
+        this.properties = new HashMap<>();
+        for (var property : state.getProperties()) {
+            this.properties.put(property.getName(), state.get(property));
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T extends Comparable<T>, V extends T> BlockState getState() {
+        var state = Registry.BLOCK.get(new Identifier(this.id)).getDefaultState();
+        for (var property : this.properties.keySet()) {
+            try {
+                state = state.with((Property<T>) state.getProperties().stream().filter(p -> p.getName().equals(property)).findFirst().orElseThrow(), (V) this.properties.get(property));
+            } catch (IllegalArgumentException ignored) {
+            }
+        }
+        return state;
     }
 }
--- src/main/java/ru/DmN/vsssl/Main.java
+++ src/main/java/ru/DmN/vsssl/Main.java
@@ -3,21 +3,21 @@ package ru.DmN.vsssl;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.minecraft.command.argument.BlockPosArgumentType;
-import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.registry.Registry;
+
+import java.io.*;
 
 import static net.minecraft.server.command.CommandManager.argument;
 import static net.minecraft.server.command.CommandManager.literal;
 
 public class Main implements ModInitializer {
-    public static RegionData save;
+    public static byte[] save;
 
     @Override
     public void onInitialize() {
         CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
             dispatcher.register(literal("copyregion").then(argument("start", BlockPosArgumentType.blockPos()).then(argument("end", BlockPosArgumentType.blockPos()).executes(context -> {
-                save = new RegionData();
+                RegionData save = new RegionData();
                 var pos0 =  BlockPosArgumentType.getBlockPos(context, "start");
                 var pos1 = BlockPosArgumentType.getBlockPos(context, "end");
                 var xStart = Math.min(pos0.getX(), pos1.getX());
@@ -35,21 +35,45 @@ public class Main implements ModInitializer {
                         do {
                             var state = world.getBlockState(new BlockPos(i, j, k));
                             if (!state.isAir())
-                                save.blocks.add(new BlockData(Registry.BLOCK.getId(state.getBlock()).getPath(), xStart - i, yStart - j, zStart - k));
+                                save.blocks.add(new BlockData( i - xStart, j - yStart, k - zStart, state));
                             k++;
                         } while (k < zEnd);
                         j++;
                     } while (j < yEnd);
                     i++;
                 } while (i < xEnd);
+
+                try {
+                    var baos = new ByteArrayOutputStream();
+                    var oos = new ObjectOutputStream(baos);
+                    oos.writeObject(save);
+                    oos.close();
+                    baos.close();
+                    Main.save = baos.toByteArray();
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+
                 return 1;
             }))));
+
             dispatcher.register(literal("pasteregion").then(argument("pos", BlockPosArgumentType.blockPos()).executes(context -> {
+                RegionData save;
+
+                try {
+                    var bais = new ByteArrayInputStream(Main.save);
+                    var ois = new ObjectInputStream(bais);
+                    save = (RegionData) ois.readObject();
+                    ois.close();
+                    bais.close();
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+
                 var world = context.getSource().getWorld();
                 var offset = BlockPosArgumentType.getBlockPos(context, "pos");
-                for (var data : save.blocks) {
-                    world.setBlockState(offset.add(data.xOffset, data.yOffset, data.zOffset), Registry.BLOCK.get(new Identifier(data.id)).getDefaultState());
-                }
+                for (var data : save.blocks)
+                    world.setBlockState(offset.add(data.xOffset, data.yOffset, data.zOffset), data.getState());
                 return 1;
             })));
         });
